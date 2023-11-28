94eba5e46784595eeabfbace76cb34ea2b96d242
Pdibyajyotipalei <151999976+Pdibyajyotipalei@users.noreply.github.com>
2023-11-28 10:18:35 +0000

    introduction file saved

---
+++ 01-basics/test.js
@@ -0,0 +1,9 @@
+console.log("hello")
+/*
+1.javascript is a high level scripting lanuage  to  use on developing eb and mobile application efficiently
+2.we used javasript on client side as well as server side
+3.in javascript we do logical implementation in web pages
+4.in today era we use esb version of javascript,as it is latest one.
+*ESb-Echma script(vanilla javascript)
+
+*/
